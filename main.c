8793336beee82d468ad51ac5ba0346f4080d3b8b
Mitch <60634216+Salsa69@users.noreply.github.com>
2023-05-25 15:24:27 -0700

    push

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
 #include "SDL/SDL.h"
+#include "SDL/SDL_gfxPrimitives.h"
 
 #include "vector.h"
 
@@ -9,18 +12,32 @@
 #define RANDRANGE(from, to) rand()%(to-from)+from
 #define P particles[i]
 
+#define PI 3.14159265
+
 #define WIDTH 400
 #define HEIGHT 300
-#define COUNT 200
-#define GRID_SIZE 20
-#define G 12 // Universal gravitational constant for this simulation
+#define COUNT 500
+#define GRID_SIZE 20 // Size of a grid square for computations
+#define G 15 // Universal gravitational constant for this simulation
+
+SDL_Point GetLocalCursorPos(SDL_Window* window) {
+    POINT cursorPos;
+    GetCursorPos(&cursorPos);
+
+    SDL_Point windowPos;
+    SDL_GetWindowPosition(window, &windowPos.x, &windowPos.y);
+
+    SDL_Point ret = {cursorPos.x-windowPos.x, cursorPos.y-windowPos.y};
+    return ret;
+}
 
 typedef struct {
-    float posX, posY;
-    Vector velocity;
+    Vector pos, velocity;
 } Particle;
 
 int main(int argc, char* argv[]) {
+    srand(time(NULL));
+
     if (SDL_Init(SDL_INIT_EVERYTHING)) {
         fprintf(stderr, "Error initializing SDL");
         return -1;
@@ -39,10 +56,11 @@ int main(int argc, char* argv[]) {
 
     Particle particles[COUNT];
     for (int i = 0; i < COUNT; i++) {
-        Particle particle = {RANDRANGE(5, WIDTH), RANDRANGE(5, HEIGHT), {rand()%6-3, rand()%6-3}};
+        Particle particle = {RANDRANGE(5, WIDTH), RANDRANGE(5, HEIGHT), {rand()%4-1.5f, rand()%4-1.5f}};
         particles[i] = particle;
     }
 
+    Vector blackHole = {WIDTH/2.f, HEIGHT/2.f};
     BYTE running = 1;
     while(running) {
         SDL_Event event;
@@ -51,6 +69,11 @@ int main(int argc, char* argv[]) {
             switch (event.type) {
                 case SDL_QUIT:
                     running = 0;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    SDL_Point cursorPos = GetLocalCursorPos(window);
+                    blackHole.x = cursorPos.x;
+                    blackHole.y = cursorPos.y;
                     break;
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.scancode) {
@@ -67,26 +90,45 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(ren, 0, 255, 0, 255);
         for(int i = 0; i < COUNT; i++) {
-            Vector blackHole = {WIDTH/2.f, HEIGHT/2.f};
-
-            Vector v1 = {blackHole.x - P.posX, blackHole.y - P.posY};
+            Vector v1 = {blackHole.x - P.pos.x, blackHole.y - P.pos.y};
             Vector v2 = vec_normalize(v1);
 
             float distance = vec_length(v1);
-            float magnitude = 1/(fabsf(distance) < .1f ? .1f : distance) * G;
+            float magnitude = G/(fabsf(distance) < 15.f ? 15.f : distance);
             Vector a = {v2.x*magnitude, v2.y*magnitude};
 
             P.velocity.x += a.x;
             P.velocity.y += a.y;
 
-            P.posX += P.velocity.x;
-            P.posY += P.velocity.y;
+            P.pos.x += P.velocity.x;
+            P.pos.y += P.velocity.y;
 
-            SDL_RenderDrawPointF(ren, particles[i].posX, particles[i].posY);
+            if (P.pos.x < 0 || P.pos.x > 400)
+                P.velocity.x = -P.velocity.x;
+            if (P.pos.y < 0 || P.pos.y > 300)
+                P.velocity.y = -P.velocity.y;
+
+            SDL_RenderDrawPointF(ren, P.pos.x, P.pos.y);
         }
 
+        {
+            SDL_SetRenderDrawColor(ren, 255, 255, 255, 100);
+            SDL_Rect focus = {particles[0].pos.x - 2, particles[0].pos.y - 2, 5, 5};
+            SDL_RenderDrawRect(ren, &focus);
+
+            SDL_SetRenderDrawColor(ren, 255, 0, 0, 100);
+            SDL_RenderDrawLineF(ren, blackHole.x, blackHole.y,
+                                particles[0].pos.x, particles[0].pos.y);
+
+            aacircleColor(ren, blackHole.x, blackHole.y, 2, 0x64f22fff);
+            aacircleColor(ren, particles[0].pos.x, particles[0].pos.y, 2, 0x64ffffff);
+        }
+
+        printf("Speed: %.1f | Distance: %.1f\r", vec_length(particles[0].velocity),
+                                               distance(particles[0].pos, blackHole));
+
         SDL_RenderPresent(ren);
-        SDL_Delay(50);
+        SDL_Delay(30);
     }
 
     SDL_DestroyRenderer(ren);
